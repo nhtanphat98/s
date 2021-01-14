9b86e204676f7060249ed9e3abea48988eb2d71c
tanphathtc <77091702+tanphathtc@users.noreply.github.com>
2021-01-14 13:07:16 +0700

    Test

---
+++ mycode.js
@@ -0,0 +1,12 @@
+function checkClick(){
+	alert("Chính xác!!!! Em ngu vl!!");
+}
+
+function check(){
+	var x = document.getElementById("ko").value;
+	document.getElementById("ko").value = 'Có';
+}
+function check1(){
+	var x = document.getElementById("ko").value;
+	document.getElementById("ko").value = "Không";
+}
